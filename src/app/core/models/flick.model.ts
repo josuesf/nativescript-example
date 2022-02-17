f02aa0f9f6234ef7ee2416cfa72634f71a43c987
Josue Farfan Lazo <jfarfala@LIM-FVFGQ2SPQ05P.local>
2022-02-17 11:33:11 -0500

    init project and creation home component

---
+++ src/app/core/models/flick.model.ts
@@ -0,0 +1,14 @@
+// src/app/core/models/flick.model.ts
+
+export interface FlickModel {
+    id: number
+    genre: string
+    title: string
+    image: string
+    url: string
+    description: string
+    details: {
+      title: string
+      body: string
+    }[]
+  }
---
+++ src/app/features/details/details.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { FlickService } from '~/app/core/services/flick.service'
+import { FlickModel } from '~/app/core/models/flick.model'
+
+@Component({
+    moduleId: module.id,
+    selector: 'ns-details',
+    templateUrl: 'details.component.html'
+})
+export class DetailsComponent {
+    flick: FlickModel | undefined = undefined
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private flickService: FlickService
+    ) { }
+
+    ngOnInit(): void {
+        const id = +this.activatedRoute.snapshot.params.id
+        if (id) {
+            this.flick = this.flickService.getFlickById(id)
+        }
+    }
+}
